4b899309889fa8d86b1fe79c9f105d3daea686a3
hungrysamurai <kryjeka@mail.ru>
2023-04-19 12:59:28 +0300

    doc

--- src/classes.js
+++ src/classes.js
@@ -1,24 +1,59 @@
+/**
+ * Class that returns single todo item
+ */
 class ToDoItem {
+  /**
+   *
+   * @param {string} text - text of todo item
+   * @param {string} status - status of todo - active/done
+   * @this ToDoItem
+   */
   constructor(text, status) {
     this.text = text;
     this.status = status;
   }
 }
 
+/**
+ * Class that returns object with title, unique ID and array of todos objects
+ */
 export class ToDoListData {
+  /**
+ * Generates unique ID for list of todos
+ * @type {number}
+ */
+  id = Date.now() + Math.floor(Math.random() * 100000);
+
+  /**
+*
+* @param {string} title - title of list
+* @param {Array} todos - array of ToDoItem's
+* @this ToDoListData
+*/
   constructor(title, todos = []) {
     this.title = title;
     this.todos = todos;
-    this.id = Math.floor(Math.random() * 100000);
   }
 }
 
+/**
+ * Class that returns ToDoList object
+ */
 export class ToDoList {
+
+  /**
+  *
+  * @param {HTMLElement} listContainer - parent DOM element for todo list
+  * @param {HTMLElement} titleContainer - parent DOM element for title
+  * @param {Object} listData - ToDoListData object with todos
+  * @this ToDoList
+  */
   constructor(listContainer, titleContainer, listData) {
     const { title, todos, id } = listData;
 
     // Create editable title element for current list
     this.todosContainer = listContainer;
+    this.titleContainer = titleContainer;
     this.titleElement = this.createTitleElement(titleContainer);
     this.todosDOM;
 
@@ -42,113 +77,25 @@ export class ToDoList {
     this.titleElement.focus();
   }
 
-  createTitleElement(titleContainer) {
-    titleContainer.innerHTML = "";
+  /**
+   * 
+   * @property {Function} createTitleElement - creates title input and puts it to DOM 
+   * @returns {HTMLElement} 
+   */
+  createTitleElement() {
+    this.titleContainer.innerHTML = "";
 
     const newTitleEl = document.createElement("input");
     newTitleEl.classList.add("list-heading");
-
-    titleContainer.append(newTitleEl);
+    this.titleContainer.append(newTitleEl);
 
     return newTitleEl;
   }
 
-  addTitleListeners() {
-    this.titleElement.addEventListener("focus", (e) => {
-      e.target.value = this.title;
-    });
-
-    this.titleElement.addEventListener("blur", (e) => {
-      if (!e.target.value) {
-        alert("Добавьте название списка!");
-
-        this.title = "Новый список дел";
-        e.target.placeholder = this.title;
-        this.updateLSTitle(this.title);
-
-        return;
-      }
-    });
-
-    this.titleElement.addEventListener("input", (e) => {
-      this.titleElement.placeholder = "";
-      this.title = e.target.value;
-
-      // Update title of current list in localStorage
-      this.updateLSTitle(this.title);
-    });
-  }
-
-  // Add new ToDo
-  addNewToDo(text) {
-    // Check if there text at all
-    if (!text) return;
-
-    // Check if text already exist in list
-    if (this.todos.find((el) => el.text === text)) return;
-
-    this.todos.push(new ToDoItem(text, "active"));
-
-    this.updateLSToDos();
-    this.renderToDOM();
-  }
-
-  // Delete ToDo by index
-  deleteToDo(index) {
-    this.todos.splice(index, 1);
-
-    this.updateLSToDos();
-    this.renderToDOM();
-  }
-
-  editToDo(index, el) {
-    if (!el.querySelector(".todo-text p")) {
-      const newText = el.querySelector(".todo-text input").value;
-      this.todos[index].text = newText;
-
-      this.updateLSToDos();
-      this.renderToDOM();
-    } else {
-      el.querySelector(".todo-text p").remove();
-
-      const currentIcon = el.querySelector(".edit-button i");
-
-      currentIcon.classList.remove("fa-pen");
-      currentIcon.classList.add("fa-check");
-
-      const editInput = document.createElement("input");
-      editInput.setAttribute("type", "text");
-      editInput.value = this.todos[index].text;
-
-      el.querySelector(".todo-text").append(editInput);
-      editInput.focus();
-
-      editInput.addEventListener("blur", (e) => {
-        const newText = e.target.value;
-        this.todos[index].text = newText;
-
-        this.updateLSToDos();
-        this.renderToDOM();
-      });
-    }
-  }
-
-  // Mark ToDo as done
-  markAsDone(index, el) {
-    // Check if ToDo is active
-    if (this.todos[index].status === "active") {
-      el.querySelector("i").className = "far fa-light fa-circle-check";
-      el.className = "todo done";
-      this.todos[index].status = "done";
-    } else {
-      el.querySelector("i").className = "far fa-light fa-circle";
-      el.className = "todo";
-      this.todos[index].status = "active";
-    }
-
-    this.updateLSToDos();
-  }
-
+  /**
+   * @property {Function} renderToDOM - re/render todolist in DOM 
+   * @returns {void}
+   */
   renderToDOM() {
     // Reset container
     this.todosContainer.innerHTML = "";
@@ -161,6 +108,12 @@ export class ToDoList {
     this.addClickEvents();
   }
 
+  /**
+   * @property {Function} createDOMElement - generate individual todo DOM-element
+   * @param {Object} todo - ToDoItem object
+   * @param {number} index
+   * @returns {HTMLElement} - todo DOM-element
+   */
   createDOMElement(todo, index) {
     const { text, status } = todo;
 
@@ -170,9 +123,8 @@ export class ToDoList {
     container.dataset.index = index;
     container.innerHTML = `
         <button class="check-button">
-           <i class="far fa-light ${
-             status === "active" ? "fa-circle" : "fa-circle-check"
-           }"></i>
+           <i class="far fa-light ${status === "active" ? "fa-circle" : "fa-circle-check"
+      }"></i>
         </button>
 
         <div class="todo-text">
@@ -190,8 +142,14 @@ export class ToDoList {
     return container;
   }
 
+  /**
+   * @property {Function} addClickEvents - add events on generated list of todos
+   * @returns {void}
+   */
   addClickEvents() {
+
     this.todosDOM = this.todosContainer.querySelectorAll(".todo");
+
     this.todosDOM.forEach((todoEl) => {
       const editBtn = todoEl.querySelector(".edit-button");
       const deleteBtn = todoEl.querySelector(".trash-button");
@@ -229,13 +187,145 @@ export class ToDoList {
     });
   }
 
+
+  /**
+   * @property {Function} editToDo - Edit current todo
+   * @param {string} index - position of todo to edit in list
+   * @param {HTMLElement} el - DOM element of todo
+   * @returns {void}
+   */
+  editToDo(index, el) {
+    console.log(index);
+    if (!el.querySelector(".todo-text p")) {
+      const newText = el.querySelector(".todo-text input").value;
+      this.todos[index].text = newText;
+
+      this.updateLSToDos();
+      this.renderToDOM();
+    } else {
+      el.querySelector(".todo-text p").remove();
+
+      const currentIcon = el.querySelector(".edit-button i");
+
+      currentIcon.classList.remove("fa-pen");
+      currentIcon.classList.add("fa-check");
+
+      const editInput = document.createElement("input");
+      editInput.setAttribute("type", "text");
+      editInput.value = this.todos[index].text;
+
+      el.querySelector(".todo-text").append(editInput);
+      editInput.focus();
+
+      editInput.addEventListener("blur", (e) => {
+        const newText = e.target.value;
+        this.todos[index].text = newText;
+
+        this.updateLSToDos();
+        this.renderToDOM();
+      });
+    }
+  }
+
+  /**
+   * @property {Function} deleteToDo - Delete ToDo by index
+   * @param {string} index - position of todo to delete
+   * @returns {void}
+   */
+  deleteToDo(index) {
+    this.todos.splice(index, 1);
+
+    this.updateLSToDos();
+    this.renderToDOM();
+  }
+
+
+  /**
+ * @property {Function} markAsDone - Mark ToDo as done
+ * @param {string} index - position of todo to edit in list
+ * @param {HTMLElement} el - DOM element of todo
+ * @returns {void}
+ */
+  markAsDone(index, el) {
+    // Check if ToDo is active
+    if (this.todos[index].status === "active") {
+      el.querySelector("i").className = "far fa-light fa-circle-check";
+      el.className = "todo done";
+      this.todos[index].status = "done";
+    } else {
+      el.querySelector("i").className = "far fa-light fa-circle";
+      el.className = "todo";
+      this.todos[index].status = "active";
+    }
+
+    this.updateLSToDos();
+  }
+
+  /**
+ * @property {Function} addTitleListeners - set up title element events
+ * @returns {void}
+ */
+  addTitleListeners() {
+    this.titleElement.addEventListener("focus", (e) => {
+      e.target.value = this.title;
+    });
+
+    this.titleElement.addEventListener("blur", (e) => {
+      if (!e.target.value) {
+        alert("Добавьте название списка!");
+
+        this.title = "Список дел";
+
+        e.target.placeholder = this.title;
+        this.updateLSTitle(this.title);
+
+        return;
+      }
+    });
+
+    this.titleElement.addEventListener("input", (e) => {
+      this.titleElement.placeholder = "";
+      this.title = e.target.value;
+
+      // Update title of current list in localStorage
+      this.updateLSTitle(this.title);
+    });
+  }
+
+
+  /**
+* @property {Function} addNewToDo - Add new ToDo to current list
+* @param {string} text - text content of new todo
+* @returns {void}
+*/
+  addNewToDo(text) {
+    // Check if there text at all
+    if (!text) return;
+
+    // Check if text already exist in list
+    if (this.todos.find((el) => el.text === text)) return;
+
+    this.todos.push(new ToDoItem(text, "active"));
+
+    this.updateLSToDos();
+    this.renderToDOM();
+  }
+
+  /**
+    * @property {Function} updateOnDrag - Update data in ToDoList object after drag and drop in DOM
+    * @returns {void}
+    */
   updateOnDrag() {
     this.todos = this.newSort();
     this.updateLSToDos();
     this.renderToDOM();
   }
 
-  // Re arrange array
+
+  /**
+  * @property {Function} newSort - Re arrange array of todos based on current arrange in DOM
+  * @returns {Array} arranged array of todos
+  */
   newSort() {
     const sorted = [];
     const toDosInDOM = [
@@ -250,6 +340,11 @@ export class ToDoList {
     return sorted;
   }
 
+  /**
+    * @property {Function} updateLSTitle - update list title in localStorage
+    * @param {string} - new title of list
+    * @returns {void}
+    */
   updateLSTitle(newTitle) {
     // Parse localStorage
     const parsed = JSON.parse(localStorage.getItem("todoList"));
@@ -265,6 +360,10 @@ export class ToDoList {
     localStorage.setItem("todoList_active", JSON.stringify(activeList.id));
   }
 
+  /**
+  * @property {Function} updateLSToDos - update todos in localStorage
+  * @returns {void}
+  */
   updateLSToDos() {
     // Parse localStorage
     const parsed = JSON.parse(localStorage.getItem("todoList"));
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import Sortable from "sortablejs";
-import { ToDoListData, ToDoList } from "./classes.js";
+import { ToDoListData, ToDoList } from "./classes";
 
 // DOM Elements
 const { body } = document;
@@ -21,7 +21,8 @@ let currentList;
 if (!localStorage.getItem("todoList")) initLocalStorage();
 else loadLocalStorage();
 
-// Init Sortable
+
+// Init Sortable 
 const sortable = new Sortable(currentTodosContainer, {
   animation: 300,
   delay: 250,
@@ -30,24 +31,7 @@ const sortable = new Sortable(currentTodosContainer, {
     currentList.updateOnDrag();
   },
   onChoose: function (e) {
-    e.item.classList.add("active");
-    const todoTop = e.originalEvent.offsetY;
-    const todoLeft = e.originalEvent.offsetX;
-
-    const chosenMark = document.createElement("span");
-    chosenMark.classList.add("chosen");
-
-    if (todoTop) {
-      chosenMark.style.top = todoTop + "px";
-      chosenMark.style.left = todoLeft + "px";
-    } else {
-      chosenMark.style.top = "50%";
-      chosenMark.style.left = "50%";
-    }
-
-    e.item.append(chosenMark);
-
-    setTimeout(() => chosenMark.remove(), 200);
+    highlightToDo(e);
   },
 });
 
@@ -70,13 +54,10 @@ addNewListBtn.addEventListener("click", () => {
   }
 });
 
-function toggleListOfLists() {
-  overlay.classList.toggle("hidden");
-  allListsContainer.classList.toggle("hidden");
-  body.classList.toggle("no-scroll");
-}
-
-// Init/update list of lists element
+/**
+ * @property {Function} updateListOfLists - Init/update list of lists element
+ * @returns {void}
+ */
 function updateListOfLists() {
   // Clear element
   listOfListsEl.innerHTML = "";
@@ -116,24 +97,11 @@ function updateListOfLists() {
   });
 }
 
-function addNewList() {
-  // Parse current current localStorage
-  const parsed = JSON.parse(localStorage.getItem("todoList"));
-
-  // Init new list
-  const newList = new ToDoListData("Новый список дел");
-
-  parsed.push(newList);
-
-  currentList = new ToDoList(currentTodosContainer, titleContainer, newList);
-
-  localStorage.setItem("todoList", JSON.stringify(parsed));
-  localStorage.setItem("todoList_active", JSON.stringify(newList.id));
-
-  updateListOfLists();
-  todoInput.value = "";
-}
-
+/**
+ * @property {Function} changeList - Set current list to new
+ * @param {string} id - id of list
+ * @returns {void}
+ */
 function changeList(id) {
   const targetID = id;
 
@@ -148,30 +116,77 @@ function changeList(id) {
   todoInput.value = "";
 }
 
+/**
+ * @property {Function} toggleListOfLists - Toggle list of lists container in DOM
+ * @returns {void}
+ */
+function toggleListOfLists() {
+  overlay.classList.toggle("hidden");
+  allListsContainer.classList.toggle("hidden");
+  body.classList.toggle("no-scroll");
+}
+
+/**
+ * @property {Function} addNewList - Adds new list in list of lists and set it in localStorage
+ * @returns {void}
+ */
+function addNewList() {
+  // Parse current current localStorage
+  const parsed = JSON.parse(localStorage.getItem("todoList"));
+  const listCount = parsed.length;
+  // Init new list
+  const newList = new ToDoListData(`Новый список дел ${listCount + 1}`);
+
+  parsed.push(newList);
+
+  currentList = new ToDoList(currentTodosContainer, titleContainer, newList);
+
+  localStorage.setItem("todoList", JSON.stringify(parsed));
+  localStorage.setItem("todoList_active", JSON.stringify(newList.id));
+
+  updateListOfLists();
+  todoInput.value = "";
+}
+
+/**
+ * @property {Function} deleteList - delete list by passed id
+ * @param {PointerEvent} e - event object than comes from click on delete button
+ * @returns {void}
+ */
 function deleteList(e) {
+  console.log(e);
   const targetID = e.target.dataset.listid;
 
   const parsed = JSON.parse(localStorage.getItem("todoList"));
   const targetList = parsed.find((list) => list.id === +targetID);
   const index = parsed.indexOf(targetList);
 
+  // if current list is list to delete and it is not last list
   if (currentList.id === targetList.id) {
     if (parsed.length - 1 > 0) {
       let prevID = parsed[index - 1].id;
+      // make previous list active list
       changeList(prevID);
     }
   }
 
   parsed.splice(index, 1);
 
+  // if list to delete is last list - init new empty list
   if (parsed.length === 0) {
     initLocalStorage();
   } else {
     localStorage.setItem("todoList", JSON.stringify(parsed));
   }
+
   updateListOfLists();
 }
 
+
+/**
+ * @property {Function} initLocalStorage - init 'todoList' & 'todoList_active' entries in localStorage, set up empty list
+ * @returns {void}
+ */
 function initLocalStorage() {
   // Init new empty list data structure
   const newListData = new ToDoListData("Новый список дел");
@@ -193,10 +208,13 @@ function initLocalStorage() {
   updateListOfLists();
 }
 
+/**
+ * @property {Function} loadLocalStorage - retrieve data from localStorage
+ * @returns {void}
+ */
 function loadLocalStorage() {
   // Parse localStorage
   const parsed = JSON.parse(localStorage.getItem("todoList"));
-  // allTitles = parsed.map(list => [list.title]);
 
   // Find active list
   const activeListSaved = JSON.parse(localStorage.getItem("todoList_active"));
@@ -205,15 +223,42 @@ function loadLocalStorage() {
   // Set current list to active list
   currentList = new ToDoList(currentTodosContainer, titleContainer, activeList);
 
-  // Add new todo to current list
+  // Initially - set up form to add new items to current list
   inputForm.addEventListener("submit", activateForm);
 
   updateListOfLists();
 }
 
+/**
+ * @property {Function} activateForm - Set up form to add new items to current list
+ * @param {SubmitEvent} e - event object from "submit" event 
+ * @returns {void}
+ */
 function activateForm(e) {
   e.preventDefault();
 
   currentList.addNewToDo(todoInput.value);
   todoInput.value = "";
+}
+
+/**
+ * @property {Function} highlightToDo - Highlight choosen todo item
+ * @param {CustomEvent} e - event object that comes from Sortable event fired when item become active
+ */
+function highlightToDo(e) {
+
+  e.item.classList.add("active");
+
+  const todoTop = e.originalEvent.layerY;
+  const todoLeft = e.originalEvent.layerX;
+
+  const chosenMark = document.createElement("span");
+  chosenMark.classList.add("chosen");
+
+  chosenMark.style.top = todoTop + "px";
+  chosenMark.style.left = todoLeft + "px";
+
+  e.item.append(chosenMark);
+
+  setTimeout(() => chosenMark.remove(), 300);
 }
